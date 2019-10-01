6149472fd0870074746282245afde52945e66a03
Oscar Maybon <oscar.maybon@gmail.com>
2019-10-01 14:58:14 +0200

    gem update

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,7 @@
 import "bootstrap";
+import "../components/mousewheel";
+import "../components/nanoscroller";
+import "../components/francemap";
+import "../components/jquery";
+global.$ = jQuery;
+
